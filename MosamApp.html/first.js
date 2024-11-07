3e4e275f8b682a138bd0129114c575b88f3b600e
Ritika Bisht <128315034+ritika18b@users.noreply.github.com>
2024-11-07 11:09:09 +0530

    Add files via upload

---
+++ MosamApp.html/first.js
@@ -0,0 +1,16 @@
+//words vs keywords
+//var const let
+//hoisting - var ko banane se phale use karna , variable and functions are hoisted which means
+// there dedlaration is moved on the top of code
+//var a=12;
+//var a;
+//a=12;
+/*   not defined- jis ka existence nahi hai code me
+undefined-jis ka existance hai par value nahi pata 
+Types in JS: Premetive -> number, string, null, undefined, boolean and reference -> [] () {} 
+aisi koi bhi value jisko copy karne par real copy nahi hota, balki us main value ka reference pass hojata hai,
+use hum reference value kahte hai. Jiska copy karne par real copy ho jaaye wo value primitive type value hoti hai
+
+conditionals: if else else-if 
+loop: for while 
+*/
